249ca760d9ad4efd4ae5f119712a7c7b07e34edc
matt <mattsnaccc@gmail.com>
2021-08-15 18:33:29 -0700

    chore(analytics): splitbee

---
+++ src/pages/_document.tsx
@@ -0,0 +1,28 @@
+import Document, {
+    DocumentContext,
+    Head,
+    Html,
+    Main,
+    NextScript,
+} from "next/document";
+
+class MyDocument extends Document {
+    static async getInitialProps(ctx: DocumentContext) {
+        const initialProps = await Document.getInitialProps(ctx);
+        return await Document.getInitialProps(ctx);
+    }
+    render() {
+        return (
+            <Html lang="en">
+                <Head>
+                    <script async src="https://cdn.splitbee.io/sb.js" />
+                </Head>
+                <body>
+                <Main />
+                <NextScript />
+                </body>
+            </Html>
+        );
+    }
+}
+export default MyDocument;
